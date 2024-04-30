5f5f630067b636a8f27beff3b13bb98e1b2227cd
counts-cod0m@icloud.com <counts-cod0m@icloud.com>
2024-04-30 16:38:54 +0000

    Created Form2.RowTemplate1; Edited settings, Form2 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -7,12 +7,20 @@ class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    #self.add_card(10)
-    dg = DataGrid(rows_per_page=3)
-    drp = DataRowPanel()
-    drp.add_component(TextBox)
-    rp = RepeatingPanel()
-    rp.item_template = DataRowPanel
-    rp.items = [1,2,3,4,5,6,7,8,9]
-    dg.add_component(rp)
-    self.add_component(dg)
+
+
+    # Create your Data Grid 
+    grid = DataGrid()
+    grid.rows_per_page = 3
+    # Add the Data Grid to your Form
+    self.add_component(grid)
+
+    # Add two columns to your Data Grid
+
+
+    rp = RepeatingPanel(item_template=Card)
+    # Set its items property
+    rp.items = [1,2,3,4,5,6,7,8,9
+    ]
+    # Add the repeating panel to your data grid
+    grid.add_component(rp)
