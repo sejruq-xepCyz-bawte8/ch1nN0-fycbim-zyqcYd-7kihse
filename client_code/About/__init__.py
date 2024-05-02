352b72bfa232f41dc45e6505905c7f0a96cd2f80
counts-cod0m@icloud.com <counts-cod0m@icloud.com>
2024-05-02 11:50:05 +0000

    Created _index; Edited settings and About

--- client_code/About/__init__.py
+++ client_code/About/__init__.py
@@ -1,12 +1,19 @@
 from ._anvil_designer import AboutTemplate
 from anvil import *
-from anvil_extras import routing
+from time import time
 
 
-@routing.route('about')
 class About(AboutTemplate):
-  def __init__(self, **properties):
+  def __init__(self, t0, **properties):
     # Set Form properties and Data Bindings.
+    self.t0 = t0
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def form_show(self, **event_args):
+    t1 = time()
+    dt = t1 - self.t0
+    print(self.t0)
+    print(t1)
+    print(dt)
