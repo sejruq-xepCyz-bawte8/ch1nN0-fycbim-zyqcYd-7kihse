4d5f42b2c08340be526134a5f9eeb52c84ea3ebb
counts-cod0m@icloud.com <counts-cod0m@icloud.com>
2024-05-07 13:43:07 +0000

    qu

--- client_code/Author/New/__init__.py
+++ client_code/Author/New/__init__.py
@@ -2,29 +2,25 @@ from ._anvil_designer import NewTemplate
 from anvil import *
 from ...Cheteme.ElementsHtml.Icon import Icon
 from anvil_extras import ChipsInput
-from anvil.js.window import Quill
+import anvil.js
 
 class New(NewTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    editor = self.dom_nodes['editor']
-    self.quill = Quill( editor, {
-        'modules': {
-          'toolbar': "#toolbar",
-          'counter': {'container': '#counter','unit': 'word'},
-          
-        },
-        'theme': 'snow'
-    })
+    self.quill = None
+
     
 
   def post(self, *event):
+    if not self.quill :
+      from anvil.js.window import quill
+      self.quill = quill
     self.words = int(self.dom_nodes['counter'].innerText)
     self.text = self.quill.getText()
-    #self.html = self.quill.getSemanticHTML()
-    print(self.html)
-    print(self.words)
+    self.html_content = self.quill.getSemanticHTML()
+    
+    print(self.words, self.html_content)
     
 
     # Any code you write here will run before the form opens.
