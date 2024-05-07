1b3082ad553202d06a45857cddc1c30fb834e043
Ridensium <119128079+gitridensium@users.noreply.github.com>
2024-05-07 12:32:25 +0100

    icons and genres

--- client_code/Cheteme/Memory/Genres_Module.py
+++ client_code/Cheteme/Memory/Genres_Module.py
@@ -28,90 +28,96 @@ class Genres:
     return self.collection.chain().find({ 'genre' : genre }).data({'removeMeta':True})
 
 genres = [
+# to be used if collection of works in one file
+{'bg':'проза','gid':'prose', 'level':0,'desc':''},
+{'bg':'поезия','gid':'poetry', 'level':0,'desc':''},
+{'bg':'сборник проза','gid':'prose_collection', 'level':0,'desc':''},
+{'bg':'сборник поезия','gid':'poetry_collection', 'level':0,'desc':''}, 
 
-{'bg':'Микро Разказ','gid':11, 'level':1,'desc':''}, #microfiction up to 100 words
-{'bg':'Флашфикшън','gid':12, 'level':1,'desc':''}, #Short Short Stories (100 to 1,000 words) flashfiction
-{'bg':'Разказ','gid':13, 'level':1,'desc':''}, #Short Stories (1,000 to 7,500 words)
-{'bg':'Разказ','gid':14, 'level':1,'desc':''}, #Short Stories (1,000 to 7,500 words)
+# level 1
+#prose
+    {'bg':'микро разказ','gid':'microfiction', 'level':1,'desc':''}, #up to 100 words
+    {'bg':'флашфикшън','gid':'flashfiction', 'level':1,'desc':''}, #100 to 1,000 words
+    {'bg':'разказ','gid':'storie', 'level':1,'desc':''}, #1,000 to 7,500 words
+    {'bg':'повест','gid':'novelette', 'level':1,'desc':''}, #7,500 то 17,500words
+    {'bg':'новела','gid':'novella', 'level':1,'desc':''}, #17,500 to 40,000 words
+    {'bg':'роман','gid':'novel', 'level':1,'desc':''}, #40к - 100к
+    {'bg':'епопея','gid':'epic_novel', 'level':1,'desc':''}, #100к
+# poetry
+    {'bg':'стих', 'gid':'poetry', 'level':1,'desc':''},
+    {'bg':'хайку', 'gid':'haiku', 'level':1,'desc':''},
+    {'bg':'епиграма', 'gid':'epigram', 'level':1,'desc':''},
+    {'bg':'сонет', 'gid':'sonnet', 'level':1,'desc':''},
+    {'bg':'балада', 'gid':'balad', 'level':1,'desc':''},
+    {'bg':'елегия', 'gid':'elegy', 'level':1,'desc':''},
+    {'bg':'ода', 'gid':'ode', 'level':1,'desc':''},
+    {'bg':'поема', 'gid':'poem', 'level':1,'desc':''},
+    {'bg':'епос', 'gid':'epic_poetry', 'level':1,'desc':''},
 
-
-{'bg':'Новела','gid':30, 'level':1,'desc':''}, #Novellas (17,500 to 40,000 words)
-
-{'bg':'Роман','gid':70, 'level':1,'desc':''},
-  
-{'bg':'Стихотворение', 'gid':70, 'level':1,'desc':''},
-{'bg':'Хаику', 'gid':71, 'level':1,'desc':''},
-
-
-{'bg':'Фентъзи','gid':11, 'level':2,'desc':''},
-{'bg':'Фантастика','gid':12, 'level':2,'desc':''},
-{'bg':'Ужаси','gid':14, 'level':2,'desc':''},
-{'bg':'Драма','gid':15, 'level':2,'desc':''},
-{'bg':'Трилър','gid':16, 'level':2,'desc':''},
-{'bg':'Крими','gid':17, 'level':2,'desc':''},
-{'bg':'Исторически','gid':18, 'level':2,'desc':''},
-{'bg':'Романс','gid':19, 'level':2,'desc':''},
-{'bg':'Детски','gid':20, 'level':2,'desc':''},
-{'bg':'Съвременни','gid':21, 'level':2,'desc':''},
-{'bg':'Хумор','gid':22, 'level':2,'desc':''},
-{'bg':'Приключенски','gid':23, 'level':2,'desc':''},
-{'bg':'Действителни','gid':24, 'level':2,'desc':''},
-{'bg':'Еротика','gid':25, 'level':2,'desc':''},
-
-  #scifi12
-{'bg':'научна', 'genre':12, 'gid':1210, 'level':3,'desc':'Научна фантастика'},
-{'bg':'класическа','genre':12, 'gid':1212, 'level':3,'desc':'Класическа фантастика'},
-{'bg':'киберпънк','genre':12, 'gid':1213, 'level':3,'desc':'Киберпънк фантастика'},
-{'bg':'соларпънк','genre':12, 'gid':1214, 'level':3,'desc':'Соларпънк фантастика'},
-{'bg':'стиймпънк','genre':12, 'gid':1215, 'level':3,'desc':'Стиймпънк фантастика'},
-{'bg':'утопия','genre':12, 'gid':1216, 'level':3,'desc':'Фантастика - утопия'},
-{'bg':'антиутопия','genre':12, 'gid':1217, 'level':3,'desc':'Фантастика - антиутопия'},
-{'bg':'постапокалипсис','genre':12, 'gid':1218, 'level':3,'desc':'Постапокалиптична фантастика'},
-{'bg':'биопънк','genre':12, 'gid':1219, 'level':3,'desc':'Биопънк фантастика'},
-{'bg':'технотрилър','genre':12, 'gid':1220, 'level':3,'desc':'Фантастика технотрилър'},
-{'bg':'шпионска','genre':12, 'gid':1221, 'level':3,'desc':'Шпионска фантастика'},
-{'bg':'хумористична','genre':12, 'gid':1222, 'level':3,'desc':'Хумористична фантастика'},
-{'bg':'супергерои','genre':12, 'gid':1223, 'level':3,'desc':'Фантастика със супергерои'},
-{'bg':'военна','genre':12, 'gid':1224, 'level':3,'desc':'Военна фантастика'},
-
-  #cryme17
-{'bg':'Детективски','genre':17, 'gid':1710, 'level':3,'desc':'Детективско криминале'},
-{'bg':'Психопати и убийци','genre':17, 'gid':1711, 'level':3,'desc':'Криминале с психопати убийци'},
-{'bg':'Мистерии','genre':17, 'gid':1712, 'level':3,'desc':'Криминале с мистерии'},
-
-#thriller16
-{'bg':'Психопати и серийни убийци','genre':16, 'gid':1610, 'level':3,'desc':'Трилър с психопати убийци'},
-{'bg':'Криминален','genre':16, 'gid':1611, 'level':3,'desc':'Криминален трилър'},
-{'bg':'Техно','genre':16, 'gid':1612, 'level':3,'desc':'Техно трилър'},
-{'bg':'Исторически','genre':16, 'gid':1613, 'level':3,'desc':'Исторически трилър'},
-{'bg':'Психологически','genre':16, 'gid':1614, 'level':3,'desc':'Психологически трилър'},
-{'bg':'Медицински','genre':16, 'gid':1615, 'level':3,'desc':'Медицински трилър'},
-{'bg':'Шпионски','genre':16, 'gid':1616, 'level':3,'desc':'Шпионски трилър'},
-{'bg':'Политически','genre':16, 'gid':1617, 'level':3,'desc':'Политически трилър'},
-{'bg':'Романтичен','genre':16, 'gid':1618, 'level':3,'desc':'Романтичен трилър'},
-
-
-  #contemp21
-{'bg':'Експериментална','genre':21, 'gid':2110, 'level':3,'desc':''},
-{'bg':'Поток на мисълта','genre':21, 'gid':2111, 'level':3,'desc':''},
-{'bg':'Епистоларна','genre':21, 'gid':2112, 'level':3,'desc':''},
-{'bg':'Мемоари','genre':21, 'gid':2113, 'level':3,'desc':''},
-{'bg':'ЛГТБ+','genre':21, 'gid':2114, 'level':3,'desc':''},
-{'bg':'Пътепис','genre':21, 'gid':2115, 'level':3,'desc':''},
-{'bg':'Гурме','genre':21, 'gid':2116, 'level':3,'desc':''},
-{'bg':'ЧикЛит','genre':21, 'gid':2117, 'level':3,'desc':''},
-{'bg':'Феминизъм','genre':21, 'gid':2118, 'level':3,'desc':''},
-
-  #drama15
-{'bg':'Историческа драма','genre':15, 'gid':1510, 'level':3,'desc':''},
-{'bg':'Военна драма','genre':15, 'gid':1511, 'level':3,'desc':''},
-{'bg':'Любовна драма','genre':15, 'gid':1512, 'level':3,'desc':''},
-{'bg':'Семейна сага','genre':15, 'gid':1513, 'level':3,'desc':''},
-{'bg':'Политическа драма','genre':15, 'gid':1514, 'level':3,'desc':''},
-
-#historical18
-{'bg':'Исторически по действителни събития','genre':18, 'gid':1810, 'level':3,'desc':''},
-{'bg':'Историческа фикция','genre':18, 'gid':1811, 'level':3,'desc':''},
-{'bg':'Исторически романс','genre':18, 'gid':1812, 'level':3,'desc':''},
-{'bg':'Исторически трилър','genre':18, 'gid':1813, 'level':3,'desc':''},
-        ]
+#genres - level2
+    {"bg":"еротика","gid":"erotica","level":2,"desc":""},
+    {"bg":"фентъзи","gid":"fantasy","level":2,"desc":""},
+    {"bg":"фантастика","gid":"scifi","level":2,"desc":""},
+    {"bg":"ужаси","gid":"horror","level":2,"desc":""},
+    {"bg":"драма","gid":"drama","level":2,"desc":""},
+    {"bg":"трилър","gid":"thriller","level":2,"desc":""},
+    {"bg":"крими","gid":"crime","level":2,"desc":""},
+    {"bg":"исторически","gid":"historical","level":2,"desc":""},
+    {"bg":"романс","gid":"romance","level":2,"desc":""},
+    {"bg":"детски","gid":"children","level":2,"desc":""},
+    {"bg":"съвременни","gid":"contemporary","level":2,"desc":""},
+    {"bg":"хумор","gid":"humor","level":2,"desc":""},
+    {"bg":"приключенски","gid":"adventure","level":2,"desc":""},
+    {"bg":"действителни","gid":"realistic","level":2,"desc":""},
+#subgenres level3
+#scifi
+    {"bg":"научна","genre":"scifi","gid":"scifi-science","level":3,"desc":"Научна фантастика"},
+    {"bg":"класическа","genre":"scifi","gid":"scifi-classic","level":3,"desc":"Класическа фантастика"},
+    {"bg":"киберпънк","genre":"scifi","gid":"scifi-cyberpunk","level":3,"desc":"Киберпънк фантастика"},
+    {"bg":"соларпънк","genre":"scifi","gid":"scifi-solarpunk","level":3,"desc":"Соларпънк фантастика"},
+    {"bg":"стиймпънк","genre":"scifi","gid":"scifi-steampunk","level":3,"desc":"Стиймпънк фантастика"},
+    {"bg":"утопия","genre":"scifi","gid":"scifi-utopia","level":3,"desc":"Фантастика - утопия"},
+    {"bg":"антиутопия","genre":"scifi","gid":"scifi-dystopia","level":3,"desc":"Фантастика - антиутопия"},
+    {"bg":"постапокалипсис","genre":"scifi","gid":"scifi-postapocalyptic","level":3,"desc":"Постапокалиптична фантастика"},
+    {"bg":"биопънк","genre":"scifi","gid":"scifi-biopunk","level":3,"desc":"Биопънк фантастика"},
+    {"bg":"технотрилър","genre":"scifi","gid":"scifi-technothriller","level":3,"desc":"Фантастика технотрилър"},
+    {"bg":"шпионска","genre":"scifi","gid":"scifi-spy","level":3,"desc":"Шпионска фантастика"},
+    {"bg":"хумористична","genre":"scifi","gid":"scifi-humorous","level":3,"desc":"Хумористична фантастика"},
+    {"bg":"супергерои","genre":"scifi","gid":"scifi-superheroes","level":3,"desc":"Фантастика със супергерои"},
+    {"bg":"военна","genre":"scifi","gid":"scifi-military","level":3,"desc":"Военна фантастика"},
+#crime
+    {"bg":"детективски","genre":"crime","gid":"crime-detective","level":3,"desc":"Детективско криминале"},
+    {"bg":"психопати и убийци","genre":"crime","gid":"crime-psychopaths","level":3,"desc":"Криминале с психопати убийци"},
+    {"bg":"мистерии","genre":"crime","gid":"crime-mysteries","level":3,"desc":"Криминале с мистерии"},
+#thriller
+    {"bg":"психопати и серийни убийци","genre":"thriller","gid":"thriller-psychopaths","level":3,"desc":"Трилър с психопати убийци"},
+    {"bg":"криминален","genre":"thriller","gid":"thriller-crime","level":3,"desc":"Криминален трилър"},
+    {"bg":"техно","genre":"thriller","gid":"thriller-techno","level":3,"desc":"Техно трилър"},
+    {"bg":"исторически","genre":"thriller","gid":"thriller-historical","level":3,"desc":"Исторически трилър"},
+    {"bg":"психологически","genre":"thriller","gid":"thriller-psychological","level":3,"desc":"Психологически трилър"},
+    {"bg":"медицински","genre":"thriller","gid":"thriller-medical","level":3,"desc":"Медицински трилър"},
+    {"bg":"шпионски","genre":"thriller","gid":"thriller-spy","level":3,"desc":"Шпионски трилър"},
+    {"bg":"политически","genre":"thriller","gid":"thriller-political","level":3,"desc":"Политически трилър"},
+    {"bg":"романтичен","genre":"thriller","gid":"thriller-romantic","level":3,"desc":"Романтичен трилър"},
+#contemporary
+    {"bg":"експериментална","genre":"contemporary","gid":"contemporary-experimental","level":3,"desc":""},
+    {"bg":"поток на мисълта","genre":"contemporary","gid":"contemporary-consciousness_stream","level":3,"desc":""},
+    {"bg":"епистоларна","genre":"contemporary","gid":"contemporary-epistolary","level":3,"desc":""},
+    {"bg":"мемоари","genre":"contemporary","gid":"contemporary-memoirs","level":3,"desc":""},
+    {"bg":"лгтб+","genre":"contemporary","gid":"contemporary-lgbt","level":3,"desc":""},
+    {"bg":"пътепис","genre":"contemporary","gid":"contemporary-travelogue","level":3,"desc":""},
+    {"bg":"гурме","genre":"contemporary","gid":"contemporary-culinary","level":3,"desc":""},
+    {"bg":"чиклит","genre":"contemporary","gid":"contemporary-chicklit","level":3,"desc":""},
+    {"bg":"феминизъм","genre":"contemporary","gid":"contemporary-feminism","level":3,"desc":""},
+#drama
+    {"bg":"историческа драма","genre":"drama","gid":"drama-historical","level":3,"desc":""},
+    {"bg":"военна драма","genre":"drama","gid":"drama-military","level":3,"desc":""},
+    {"bg":"любовна драма","genre":"drama","gid":"drama-romance","level":3,"desc":""},
+    {"bg":"семейна сага","genre":"drama","gid":"drama-family_saga","level":3,"desc":""},
+    {"bg":"политическа драма","genre":"drama","gid":"drama-political","level":3,"desc":""},
+#historical
+    {"bg":"исторически по действителни събития","genre":"historical","gid":"historical-true_events","level":3,"desc":""},
+    {"bg":"историческа фикция","genre":"historical","gid":"historical-fiction","level":3,"desc":""},
+    {"bg":"исторически романс","genre":"historical","gid":"historical-romance","level":3,"desc":""},
+    {"bg":"исторически трилър","genre":"historical","gid":"historical-thriller","level":3,"desc":""}
+]
