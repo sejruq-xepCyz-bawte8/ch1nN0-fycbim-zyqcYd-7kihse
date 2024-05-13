8e29439d4a23e2e9f7ce24f3c470e9fe9723cfe3
Ridensium <119128079+gitridensium@users.noreply.github.com>
2024-05-13 18:45:18 +0100

    log

--- client_code/Cheteme/Main.py
+++ client_code/Cheteme/Main.py
@@ -6,6 +6,11 @@ from time import sleep, time
 from .ElementsHtml.Navigation import NavigationClass
 from .ElementsHtml.Blanket import Blanket
 
+import anvil.users
+
+user = None
+navigation = None
+
 navigation_list = [
   {'bg':'Днес', 'group':'Reader', 'form':'Today'},
   {'bg':'Творби', 'group':'Reader', 'form':'Works'},
@@ -24,12 +29,13 @@ navigation_groups = (x['group'] for x in navigation_list)
 navigation_forms = (x['form'] for x in navigation_list)
 navigation_keys = list(navigation_groups) + list(navigation_forms)
 
-user = None
+
 
 def ready(event):
+  global navigation
   print("Ready")
   navigation = NavigationClass(navigation_list=navigation_list)
-  
+  update_user()
   
   today = document.getElementById("Reader.Today")
   
@@ -40,19 +46,31 @@ def ready(event):
   #blanket.toggle()
   
   
-def navigation_click(proxyobject):
+def navigation_click(proxyobject): #will work on every form
   classes_click = proxyobject.id.split('.')
-  
   proxyobject.parentNode.classList.remove(*navigation_keys)
   proxyobject.parentNode.classList.add(*classes_click)
-  
-  
+  jQ('.ch-icon-container').removeClass('clicked')
+  proxyobject.classList.add('clicked')
   try:
         open_form(proxyobject.id)
   except:
         open_form('Reader.Today')
 
  
+def update_user():
+    user = anvil.users.get_user()
+    if user:
+        navigation.css_remove_class('not_user')
+        navigation.css_remove_class('is_author')
+        navigation.css_add_class("is_user")
+        if user.is_author:
+            navigation.css_add_class("is_author")
+    else:
+        navigation.css_remove_class('is_user')
+        navigation.css_remove_class('is_author')
+        navigation.css_add_class('not_user')
+    
         
 
 
--- client_code/Reader/Login/__init__.py
+++ client_code/Reader/Login/__init__.py
@@ -2,16 +2,55 @@ from ._anvil_designer import LoginTemplate
 from anvil import *
 import anvil.server
 import anvil.users
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
+from ...Cheteme.Main import navigation_click, user
 
+choise = {
+  True: "Регистрация",
+  False: "Вход"
+}
 
 class Login(LoginTemplate):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    from ...Cheteme.Main import navigation_click
+    
     self.navigation_click = navigation_click
     self.init_components(**properties)
+    self.signup = CheckBox(text = "Регистрация")
+    self.signup.add_event_handler('change', self.change_choise)
+    self.email = TextBox(placeholder='ел. поща')
+    self.password = TextBox(placeholder='парола')
+    self.code = TextBox(placeholder='регистр. код', visible = False)
+    self.login = Link(text="Вход")
+    self.login.add_event_handler('click', self.login_parser)
+    self.info = Label()
+    
+    self.add_component(self.signup)
+    self.add_component(self.email)
+    self.add_component(self.password)
+    self.add_component(self.code)
+    self.add_component(self.login)
+    self.add_component(self.info)
+  
+  def change_choise(self, sender, **event):
+    self.code.visible = not self.code.visible
+    self.login.text = choise[self.code.visible]
+    self.info.text = ""
 
-    # Any code you write here will run before the form opens.
+  def login_parser(self, sender, **event):
+    if self.code.visible:
+      self.sign_up_user()
+    else:
+      self.login_user()
+      
+
+  def sign_up_user(self):
+     pass
+
+  def login_user(self):
+      global user
+      try:
+        user = anvil.users.login_with_email(self.email.text, self.password.text)
+      except anvil.users.TooManyPasswordFailures as e:
+          # Do something in response to the exception
+          self.info.text = "Твърде много опити"
+      except anvil.users.AuthenticationFailed as e:
+          self.info.text = "🙀 Некоректна парола или ел. поща"
