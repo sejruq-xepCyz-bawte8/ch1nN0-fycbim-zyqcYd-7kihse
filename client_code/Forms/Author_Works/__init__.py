645e742aac01e34ce33db3e71a73d28d95b25261
counts-cod0m@icloud.com <counts-cod0m@icloud.com>
2024-06-09 03:48:11 +0000

    Edited Forms.Author_Works

--- client_code/Forms/Author_Works/__init__.py
+++ client_code/Forms/Author_Works/__init__.py
@@ -19,9 +19,12 @@ class Author_Works(_FormTemplate):
 
   def draw_works(self, **event):
       self.clear()
-      works = EDITOR.all_works_data()
-      if not works :
+      works = []
+      try:
+        works = EDITOR.all_works_data()
+      except :
         works = []
+        
       for work in works:
         container = self.add_flowpanel()
         
