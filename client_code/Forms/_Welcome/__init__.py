948673cd3bcde8190b8a31fa7ca21c95479ff0e5
Ridensium <119128079+gitridensium@users.noreply.github.com>
2024-06-14 21:51:28 +0100

    Update __init__.py

--- client_code/Forms/_Welcome/__init__.py
+++ client_code/Forms/_Welcome/__init__.py
@@ -21,11 +21,11 @@ class _Welcome(_FormTemplate):
       self.navClick_by_id("#navl-Reader-ViewerW_Work")
 
  
-    self.add_div(text="ЧетеМе 0.6.1")
+    self.add_div(text="ЧетеМе 0.6.9")
     self.add_div(text="---")
     self.add_div(text="---")
     self.add_div(text="---")
-    self.add_div(text="Тази версия е само за тестване на писателските профили и пермалинкове")
+    self.add_div(text="Тази версия е само за тестване на писателския пълен цикъл")
 
 
 
