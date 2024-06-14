87c40a2b30c9dc1fc45fa2485284b0caec62fc02
Ridensium <119128079+gitridensium@users.noreply.github.com>
2024-06-14 19:31:42 +0100

    Update __init__.py

--- client_code/Forms/Editor_Publish/__init__.py
+++ client_code/Forms/Editor_Publish/__init__.py
@@ -1,9 +1,9 @@
 from anvil import *
 from .._FormTemplate import _FormTemplate
 from ...Index.App import EDITOR
-
-
-
+from ...Index.App import USER_ID
+import anvil.server
+from time import sleep
 
 class Editor_Publish(_FormTemplate):
   def __init__(self, **properties):
@@ -32,6 +32,21 @@ class Editor_Publish(_FormTemplate):
 
 
 
-  def publish(self, sender, *event):
-    pass
+  def publish(self, sender, **event):
+    print('publush click')
+
+    if USER_ID:
+      print('publush calling')
+      task = anvil.server.call('publish_author_work', html=EDITOR.html, data=EDITOR.data)
+      query = self.add_label()
+      for t in range(60):
+        sleep(1)
+        query.text=task.get_state()['message']
+        if task.is_completed(): break
+      result = task.get_return_value()
+      if result:
+         self.add_div(text='ГОТОВО всичко е успешно')
+  
+      else:
+         self.add_div(text='ПРИКЛЮЧИ но с грешки')
 
