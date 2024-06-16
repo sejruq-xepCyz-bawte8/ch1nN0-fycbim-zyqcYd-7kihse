3334bc037f9f9093d92fe8db6666169c6842bdb2
Ridensium <119128079+gitridensium@users.noreply.github.com>
2024-06-16 16:17:53 +0100

    Update __init__.py

--- client_code/Forms/Reader_Today/__init__.py
+++ client_code/Forms/Reader_Today/__init__.py
@@ -1,6 +1,6 @@
 from anvil import *
 from .._FormTemplate import _FormTemplate
-
+from ...Index.App import READER
 
 class Reader_Today(_FormTemplate):
   def __init__(self, **properties):
@@ -11,7 +11,8 @@ class Reader_Today(_FormTemplate):
 
   def show_form(self, **event):
     self.add_label(text=self.form_name)
-
+    for w in READER.today:
+      print(w)
     
     
 
