6cf343e3b79deef0ce3569f9a8b85b037927810e
Ridensium <119128079+gitridensium@users.noreply.github.com>
2024-06-16 16:56:41 +0100

    Update __init__.py

--- client_code/Forms/Reader_Today/__init__.py
+++ client_code/Forms/Reader_Today/__init__.py
@@ -16,7 +16,7 @@ class Reader_Today(_FormTemplate):
     self.cover_container = self.add_div(id='cover-container')
     for w in READER.today:
       wid = next(iter(w))
-
+      print(w)
       work_data = READER.get_work_data(wid)
       cover = CoverClass(work_data)
       self.append_jq_el(element=cover.el, parent=self.cover_container)
