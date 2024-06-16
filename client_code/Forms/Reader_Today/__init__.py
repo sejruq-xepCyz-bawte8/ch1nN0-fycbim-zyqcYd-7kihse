8e6537c5048c4a3a7a9311981ce200f6572de4e2
Ridensium <119128079+gitridensium@users.noreply.github.com>
2024-06-16 16:43:42 +0100

    Update __init__.py

--- client_code/Forms/Reader_Today/__init__.py
+++ client_code/Forms/Reader_Today/__init__.py
@@ -1,6 +1,8 @@
 from anvil import *
 from .._FormTemplate import _FormTemplate
 from ...Index.App import READER
+from ..Editor_Cover.CoverElement import CoverClass
+from anvil.js.window import jQuery as jQ
 
 class Reader_Today(_FormTemplate):
   def __init__(self, **properties):
@@ -11,10 +13,13 @@ class Reader_Today(_FormTemplate):
 
   def show_form(self, **event):
     self.add_label(text=self.form_name)
+    self.cover_container = self.add_div(id='cover-container')
     for w in READER.today:
       wid = next(iter(w))
+
       work_data = READER.get_work_data(wid)
-      self.add_div(text=work_data['title'])
+      cover = CoverClass(work_data)
+      self.append_jq_el(element=cover.el, parent=self.cover_container)
     
     
 
