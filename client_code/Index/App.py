22fa565f80993207d8e7bf3fd7dafd79f1c86091
Ridensium <119128079+gitridensium@users.noreply.github.com>
2024-06-17 18:17:56 +0100

    Update App.py

--- client_code/Index/App.py
+++ client_code/Index/App.py
@@ -91,10 +91,14 @@ def init_user()->bool:
     USER_ID = USER['user_id'] if USER else None
     USER_EMAIL = USER['email'] if USER else None
     IS_USER = bool(USER)
-    AUTHOR_ID = USER['author_id'] if USER and 'author_id' in USER else None
-    print('AUTHOR_ID',AUTHOR_ID)
-    ADULT = USER['adult'] if USER and 'adult' in USER else None
     IS_AUTHOR = USER['is_author'] if USER and 'is_author' in USER else None
+    if IS_AUTHOR and USER['author_id']:
+        AUTHOR_ID = USER['author_id']
+    else:
+        AUTHOR_ID = anvil.server.call('get_author_id')
+    
+    ADULT = USER['adult'] if USER and 'adult' in USER else None
+    
     
     EDITOR = EditorClass() if IS_AUTHOR else None
     NAVIGATION.reset()
