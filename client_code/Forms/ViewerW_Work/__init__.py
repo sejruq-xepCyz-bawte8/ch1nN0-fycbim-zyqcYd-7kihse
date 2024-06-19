d653044582173e1b07b5d514693630c757276536
Ridensium <119128079+gitridensium@users.noreply.github.com>
2024-06-19 18:57:05 +0100

    Update __init__.py

--- client_code/Forms/ViewerW_Work/__init__.py
+++ client_code/Forms/ViewerW_Work/__init__.py
@@ -75,7 +75,7 @@ class ViewerW_Work(_FormTemplate):
                         if word.startswith('src="data:image'):
                           wordSpan = document.createElement('img')
                           wordSpan.src = word.split('"')[1]
-                          wordSpan.style.maxHeight = f'{self.targetHeigth/3}px'
+                          wordSpan.style.maxHeight = '100px'
                         else:
                           wordSpan = document.createElement('span')
                           wordSpan.innerHTML = word
