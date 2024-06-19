9b7ffdbe0d0969cf241baa8e9bffefe19db417cc
Ridensium <119128079+gitridensium@users.noreply.github.com>
2024-06-19 16:53:08 +0100

    Update __init__.py

--- client_code/Forms/ViewerW_Work/__init__.py
+++ client_code/Forms/ViewerW_Work/__init__.py
@@ -78,7 +78,7 @@ class ViewerW_Work(_FormTemplate):
                             self.createNewParagraph(element)
                             self.currentParagraph.appendChild(wordSpan)
                 
-                elif element.tagName.lower() == 'h1' and self.pageNumber > 0:
+                elif element.tagName.lower() == 'h1' and self.pageNumber > 1:
                     self.createNewPage()
                     clone = element.cloneNode('true')
                     self.currentPage.appendChild(clone)
