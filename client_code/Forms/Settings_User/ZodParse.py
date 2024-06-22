4d2b1ac75e444495e5e333677d3fa146a000ab1f
Ridensium <119128079+gitridensium@users.noreply.github.com>
2024-06-22 13:50:08 +0100

    ddd

--- client_code/Forms/Settings_User/ZodParse.py
+++ client_code/Forms/Settings_User/ZodParse.py
@@ -9,13 +9,13 @@ schema_code = z.coerce.string().min(8).email().endswith("CHE")
 
 def code_zod(sender, **event):
     sender.valid = schema_email.safe_parse(sender.text).success
-    sender.border = "LightBlue" if sender.valid else "LightSalmon"
+    sender.border = "1px solid LightBlue" if sender.valid else "1px solid LightSalmon"
 
 def email_zod(sender, **event):
     sender.valid = schema_email.safe_parse(sender.text).success
-    sender.border = "LightGreen" if sender.valid else "LightSalmon"
+    sender.border = "1px solid LightGreen" if sender.valid else "1px solid LightSalmon"
     
 def password_zod(sender, **event):
     sender.valid = schema_password.safe_parse(sender.text).success
-    sender.border = "LightGreen" if sender.valid else "LightSalmon"
+    sender.border = "1px solid LightGreen" if sender.valid else "1px solid LightSalmon"
     return sender.valid
