d99abf4fb2791f8ce4e3a4ea8a61ce73b59af371
counts-cod0m@icloud.com <counts-cod0m@icloud.com>
2024-06-27 10:42:50 +0000

    Created _Experiments.StandartForm, _Experiments.CustomHtmlForm and _Experiments

---
+++ client_code/_Experiments/StandartForm/__init__.py
@@ -0,0 +1,19 @@
+from ._anvil_designer import StandartFormTemplate
+from anvil import *
+import anvil.server
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class StandartForm(StandartFormTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    self.rich_text_1.content
+    # Any code you write here will run before the form opens.
+
+  def form_show(self, **event_args):
+    """This method is called when the form is shown on the page"""
+    pass
